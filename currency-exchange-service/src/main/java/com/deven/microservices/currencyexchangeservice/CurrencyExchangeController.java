5a7a52d70c5f36b49685f2dad0bca6339cb882d7
deven33 <devendraameta33@gmail.com>
2021-02-03 08:54:48 +0530

    Spring Cloud API Gateway-1

--- currency-exchange-service/src/main/java/com/deven/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/deven/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,6 +1,5 @@
 package com.deven.microservices.currencyexchangeservice;
 
-import java.math.BigDecimal;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
