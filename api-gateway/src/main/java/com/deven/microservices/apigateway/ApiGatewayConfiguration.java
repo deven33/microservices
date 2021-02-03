cb4e14bf4dccf6ecb5a1d0562758259faf89574c
deven33 <devendraameta33@gmail.com>
2021-02-03 18:35:32 +0530

    Routing Implementation

--- api-gateway/src/main/java/com/deven/microservices/apigateway/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/com/deven/microservices/apigateway/ApiGatewayConfiguration.java
@@ -1,11 +1,6 @@
 package com.deven.microservices.apigateway;
 
-import java.util.function.Function;
-
-import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.Buildable;
-import org.springframework.cloud.gateway.route.builder.PredicateSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,17 +8,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ApiGatewayConfiguration {
 	
-//	@Bean
-//	public RouteLocator gatewayRouter( RouteLocatorBuilder builder) {
-//		Function<PredicateSpec, Buildable<Route>> routeFunction
-//			= p -> p.path("/get")
-//					.uri("http://httpbin.ori:80");/* if request comes to /get then it will
-//													redirect to http://httpbin.ori:80
-//					 							   */
-//		return builder
-//				.routes()
-//				.route(routeFunction)
-//				.build();
-//	}
+	@Bean
+	public RouteLocator gatewayRouter( RouteLocatorBuilder builder) {
+		//Pre and Post Filters provided by Spring Cloud Gateway
+		/* if request comes to /currency-exchange/ then it will
+		redirect to naming server service name currency-exchange-service */
+		System.out.println("ENTRY::"+"ApiGatewayConfiguration");
+		return builder
+				.routes()
+				.route(p -> p.path("/currency-exchange/**")
+							.filters( f -> f.addRequestHeader("MyHeader", "MyURI")
+										.addRequestParameter("param", "myparam"))
+						.uri("lb://currency-exchange-service"))
+				.route(p -> p.path("/currency-conversion/**")
+							.filters(f -> f.addRequestHeader("MyHeader2", "MyURL2")
+									 	   .addRequestParameter("para2", "v2"))
+				.uri("lb://currency-conversion-service"))
+				.build();
+	}
 
 }
